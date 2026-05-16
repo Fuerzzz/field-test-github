12766104178446d51a6d56885f5984e9efa25f95
Fuerz <108349486+Fuerzzz@users.noreply.github.com>
2026-05-16 18:14:00 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE_NAME = 'field-test-app-v1';
+const FILES = ['./', './index.html', './manifest.json'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
+});
